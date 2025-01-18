bd7e0a245dc97e212508a46ceba4120b984fd351
maahi_ve <154302147+maahive-77@users.noreply.github.com>
2025-01-18 22:59:11 +0530

    source code
    C++ is a widely used Object Oriented Programming language and is fairly easy to understand. Learning C++ programming can be simplified into: Writing your program in a text editor and saving it with correct extension(.CPP, .C, .CP) Compiling your program using a compiler or online IDE Understanding the basic terminologies.

---
+++ chapter_01/02_program.cpp
@@ -0,0 +1,9 @@
+// Description: This program prints the message "Hello, World!" to the standard output.
+#include<iostream> // header file, include the iostream library to use the standard input/output stream objects
+int main() // main function
+{
+    // std:: ye kyu use kiya? 
+    // std:: is a namespace, which is a collection of identifiers (names) that are used to identify objects, functions, and types.
+    std::cout << "Hello, World!"; // print the message "Hello, World!" to the standard output
+    return 0; // return 0 to the operating system
+}
---
+++ chapter_01/03_hello.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num = 4 ;
+    cout<< num ;  // ye 4 print kregs 
+    cout<< " hello world " << num;   // yeha bhi  hum ye kr ke num ka value le skte hai 
+    return 0;
+}
