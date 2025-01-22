02a46fece87d77c0908ddaaf859d15d4fcbac5a6
maahi_ve <154302147+maahive-77@users.noreply.github.com>
2025-01-22 23:31:23 +0530

    source code
    conditional statement  and loops

---
+++ Complete C++ DSA Course  Data Structures & Algorithms  by Maahi/source code/lecture_03/01_cnd_op.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = -4;
+    if (a>= 0)
+    {
+        cout<< "a is positive\n";
+    }else{
+        cout<<"a is negative"<<endl;
+    }
+    cout << "-----------" << endl;
+    cout << "thank you!" << endl;
+    cout << "maahi.." << endl;
+    return 0;
+}
